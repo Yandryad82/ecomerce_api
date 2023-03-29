18a2bd71c496e10c8b0913fc112a499586504d45
Yandry Almaguer <yandryad82@gmail.com>
2023-03-29 18:53:24 -0300

    Nodemailer-Terminado

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,11 +1,21 @@
 const UsersServices = require("../services/users.service");
+const Transporter = require("../utils/mailer");
 
 const createUser = async (req, res, next) => {
   try {
     const newUser = req.body;
     const result = await UsersServices.create(newUser);
+    console.log(result);
     res.status(201).json(result);
-          
+    await Transporter.sendMail({
+      from: 'yandryad82@gmail.com',
+      to: result.email,
+      subject: `Bienvenido ${result.username}`,
+      html: 
+      `<p>Hola ${result.username}. Te damos la bievenida a nuestro Ecomerce</p>`
+    })
+    .then()
+    .catch((error) => console.log(error));      
   } catch (error) {
     next(error);
   }
